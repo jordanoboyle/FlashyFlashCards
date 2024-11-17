041647b77fbc091472344126660e54c9dded10f7
jordanoboyle <jmcmcode@gmail.com>
2024-11-17 16:28:08 -0700

    needed DOM elements

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//DOM Selectors
+const cardsContainer = document.getElementById('cards-container');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const currentEl = document.getElementById('current');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('questions');
+const answerEl = document.getElementById('answer');
+const addCardBtn = document.getElementById('add');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
+
